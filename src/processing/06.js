23d3fbc3b658fad23d7e3d2751c96aaec3d2e909
Samantha Ostrowski <isphinxs@hotmail.com>
2024-12-30 13:51:01 -0500

    Day 06A successful implementation

--- src/processing/06.js
+++ src/processing/06.js
@@ -3,267 +3,184 @@ import { readIntoTextRows } from './utils.js';
 export class Day06 {
     #dir; // 0 up; 1 right; 2 down; 3 left
     #pos;
-    #guardPath;
+    #inRoom;
     
     #rows;
-    #obstacleRows;
-    #obstacleColumns;
-    #obstaclesByRow;
-    #obstaclesByColumn;
+    #numRows;
+    #numColumns;
+    #map;
 
     constructor() {
-        this.#guardPath = new Map();
+        this.#map = [];
+        this.#inRoom = true;
     }
 
     /**
      * Count the number of positions traversed by the guard
      */
     async A() {
-        this.#rows = await readIntoTextRows('./src/data/test.txt');
-        // this.#rows = await readIntoTextRows('./src/data/06.txt');
+        // this.#rows = await readIntoTextRows('./src/data/test.txt');
+        this.#rows = await readIntoTextRows('./src/data/06.txt');
 
         this.#processMap();
+        this.#numRows = this.#rows.length;
+        this.#numColumns = this.#rows[0].length;
 
-        // move guard
-        let inRoom = true;
-        let dir = this.#dir;
-        let [x, y] = this.#pos;
-        
-        while (inRoom) {
-            let hitObstacle = false;
+        while (this.#inRoom) {
+            let dir = this.#dir;
+            let [row, column] = this.#pos;
+            // console.log({ row, column, dir });
 
-            switch (dir) {
-                case 0: // up => increase x
-                    // check for an obstacle in the column
-                    if (this.#obstacleColumns.has(y)) {
-                        // check if the obstacle is above the guard
-                        const rows = [...this.#obstaclesByColumn.get(y)];
-                        for (let i = 0; i < rows.length; i++) {
-                            const row = rows[i];
-                            if (x > row) {
-                                this.#recordSquares([x, y], [row + 1, y]);
-
-                                x = row + 1; // move to one below obstacle
-                                dir = 1;
-                                hitObstacle = true;
-                                break;
-                            }
-                        }
-                    }
-
-                    if (!hitObstacle) {
-                        // exeunt
-                        this.#recordSquares([x, y], [0, y]);
-                        x = 0;
-                        inRoom = false;
-                    }
-                    break;
-                case 1: // right => increase y
-                    // check for an obstacle in the row
-                    if (this.#obstacleRows.has(x)) {
-                        // check if the obstacle is to the right of the guard
-                        const columns = [...this.#obstaclesByRow.get(x)];
-                        // console.log({ columns });
-                        for (let i = 0; i < columns.length; i++) {
-                            const column = columns[i];
-                            if (y < column) {
-                                this.#recordSquares([x, y], [x, column - 1]);
-                                y = column - 1; // move to one to the left of the obstacle
-                                dir = 2;
-                                hitObstacle = true;
-                                break;
-                            }
-                        }
-                    } 
-                    
-                    if (!hitObstacle) {
-                        // exeunt
-                        this.#recordSquares([x, y], [x, this.#rows[0].length - 1]);
-                        y = this.#rows[0].length - 1;
-                        inRoom = false;
-                    }
-                    break;
-                case 2: // down => decrease x
-                    // check for an obstacle in the column
-                    if (this.#obstacleColumns.has(y)) {
-                        // check if the obstacle is above the guard
-                        const rows = [...this.#obstaclesByColumn.get(y)];
-                        for (let i = 0; i < rows.length; i++) {
-                            const row = rows[i];
-                            if (x < row) {
-                                this.#recordSquares([row - 1, y], [x, y]);
-                                x = row - 1; // move to one above obstacle
-                                dir = 3;
-                                hitObstacle = true;
-                                break;
-                            }
-                        }
-                    } 
-                    
-                    if (!hitObstacle) {
-                        // exeunt
-                        this.#recordSquares([this.#rows.length - 1, y], [x, y]);
-                        x = this.#rows.length - 1;
-                        inRoom = false;
-                    }
-                    break;
-                case 3: // left => decrease y
-                    // check for an obstacle in the row
-                    if (this.#obstacleRows.has(x)) {
-                        // check if the obstacle is to the right of the guard
-                        const columns = [...this.#obstaclesByRow.get(x)];
-                        // console.log({ columns });
-                        for (let i = 0; i < columns.length; i++) {
-                            const column = columns[i];
-                            if (y > column) {
-                                this.#recordSquares([x, column + 1], [x, y]);
-                                y = column + 1; // move to one to the right of the obstacle
-                                dir = 0;
-                                hitObstacle = true;
-                                break;
-                            }
-                        }
-                    } 
-                    
-                    if (!hitObstacle) {
-                        // exeunt
-                        this.#recordSquares([0, y], [x, y]);
-                        x = 0;
-                        inRoom = false;
-                    }
+            if (dir === 0) {
+                this.#moveUp(row, column);
+            } else if (dir === 1) {
+                this.#moveRight(row, column);
+            } else if (dir === 2) {
+                this.#moveDown(row, column);
+            } else if (dir === 3) {
+                this.#moveLeft(row, column);
             }
 
-            console.log(`new pos: ${x}, ${y}`);
-            console.log(`new dir: ${dir}`);
+            // console.log(this.#map);
         }
 
-        const sum = this.#guardPath.get('count');
-        this.#guardPath.forEach(row => {
-            console.log(row);
+        let sum = 0;
+        this.#map.forEach(row => {
+            row.forEach(char => {
+                if (char === 'x') {
+                    sum++;
+                }
+            });
         });
-        console.log(sum);
+
+        console.log({ sum }); // 4789
         return sum;
     }
 
-    /**
-     * Start position to end position (row, column), from the lower to higher value
-     */
-    #recordSquares(start, end) {
-        // same row
-        if (start[0] === end[0]) {
-            const row = start[0];
-            for (let i = 0; i < end[0]; i++) {
-                const column = start[1] + i;
+    #moveUp(initialRow, column) {
+        let rowPos = initialRow;
 
-                if (this.#guardPath.has(row)) {
-                    const columns = this.#guardPath.get(row);
+        // console.log('move up');
 
-                    if (!columns.has(column)) {
-                        columns.add(column);
-                        this.#guardPath.set(row, columns);
-                        const count = this.#guardPath.get('count');
-                        this.#guardPath.set('count', count + 1);
-                    }
-                } else {
-                    const columns = new Set();
-                    columns.add(column);
-                    this.#guardPath.set(row, columns);
-
-                    if (this.#guardPath.has('count')) {
-                        const count = this.#guardPath.get('count');
-                        this.#guardPath.set('count', count + 1);
-                    } else {
-                        this.#guardPath.set('count', 1);
-                    }
-                }
+        for (let i = 0; i <= initialRow; i++) {
+            const row = initialRow - i;
+            const char = this.#map[row][column];
+    
+            // console.log({ row, char });
+    
+            if (char !== '#') {
+                this.#map[row][column] = 'x';
+                rowPos = row;
+            } else {
+                this.#dir = 1;
+                break;
             }
-        } else {
-            // same column
-            const column = start[1];
-            for (let i = 0; i < end[1]; i++) {
-                const row = start[0] + i;
+        }
 
-                if (this.#guardPath.has(row)) {
-                    const columns = this.#guardPath.get(row);
+        this.#pos = [rowPos, column];
 
-                    if (!columns.has(column)) {
-                        columns.add(column);
-                        this.#guardPath.set(row, columns);
-                        const count = this.#guardPath.get('count');
-                        console.log({ count });
-                        this.#guardPath.set('count', count + 1);
-                    }
-                } else {
-                    const columns = new Set();
-                    columns.add(column);
-                    this.#guardPath.set(row, columns);
-
-                    if (this.#guardPath.has('count')) {
-                        const count = this.#guardPath.get('count');
-                        console.log({ count });
-                        this.#guardPath.set('count', count + 1);
-                    } else {
-                        console.log('Setting count to 1x');
-                        this.#guardPath.set('count', 1);
-                    }
-                }
-            }
+        if (rowPos === 0) {
+            this.#inRoom = false;
         }
     }
 
-    #processMap() {
-        const obstacleRows = new Set();
-        const obstacleColumns = new Set();
-        const obstaclesByRow = new Map();
-        const obstaclesByColumn = new Map();
+    #moveDown(initialRow, column) {
+        let rowPos = initialRow;
 
+        // console.log('move down');
+
+        for (let i = 0; i < this.#numRows - initialRow; i++) {
+            const row = initialRow + i;
+            const char = this.#map[row][column];
+    
+            // console.log({ row, char });
+    
+            if (char !== '#') {
+                this.#map[row][column] = 'x';
+                rowPos = row;
+            } else {
+                this.#dir = 3;
+                break;
+            }
+        }
+
+        this.#pos = [rowPos, column];
+
+        if (rowPos === this.#numRows - 1) {
+            this.#inRoom = false;
+        }
+    }
+
+    #moveRight(row, initialColumn) {
+        let columnPos = initialColumn;
+
+        // console.log('move right');
+
+        for (let i = 0; i < this.#numColumns - initialColumn; i++) {
+            const column = initialColumn + i;
+            const char = this.#map[row][column];
+
+            // console.log({ column, char });
+
+            if (char !== '#') {
+                this.#map[row][column] = 'x';
+                columnPos = column;
+            } else {
+                this.#dir = 2;
+                break;
+            }
+        }
+
+        this.#pos = [row, columnPos];
+
+        if (columnPos === this.#numColumns - 1) {
+            this.#inRoom = false;
+        }
+    }
+    
+    #moveLeft(row, initialColumn) {
+        let columnPos = initialColumn;
+
+        // console.log('move left');
+
+        for (let i = 0; i <= initialColumn; i++) {
+            const column = initialColumn - i;
+            const char = this.#map[row][column];
+
+            // console.log({ column, char });
+
+            if (char !== '#') {
+                this.#map[row][column] = 'x';
+                columnPos = column;
+            } else {
+                this.#dir = 0;
+                break;
+            }
+        }
+
+        this.#pos = [row, columnPos];
+
+        if (columnPos === 0) {
+            this.#inRoom = false;
+        }
+    }
+
+    #processMap() {   
         this.#rows.forEach((row, rowIdx) => {
+            const processedRow = [];
+
             for (let charIdx = 0; charIdx < row.length; charIdx++) {
                 const char = row.charAt(charIdx);
-
-                if (char === '#') {
-                    if (!obstacleRows.has(rowIdx)) {
-                        obstacleRows.add(rowIdx);
-                    }
-
-                    if (!obstacleColumns.has(charIdx)) {
-                        obstacleColumns.add(charIdx);
-                    }
-
-                    if (obstaclesByRow.has(rowIdx)) {
-                        const columns = obstacles.get(rowIdx);
-                        columns.add(charIdx);
-                        obstaclesByRow.set(rowIdx, columns);
-                    } else {
-                        const columns = new Set();
-                        columns.add(charIdx);
-                        obstaclesByRow.set(rowIdx, columns);
-                    }
-
-                    if (obstaclesByColumn.has(charIdx)) {
-                        const rows = obstacles.get(charIdx);
-                        rows.add(rowIdx);
-                        obstaclesByColumn.set(charIdx, rows);
-                    } else {
-                        const rows = new Set();
-                        rows.add(rowIdx);
-                        obstaclesByColumn.set(charIdx, rows);
-                    }
-                }
 
                 if (char === '^') {
                     this.#dir = 0;
                     this.#pos = [rowIdx, charIdx];
                 }
+
+                processedRow.push(char);
             };
+
+            this.#map.push(processedRow);
         });
-
-        this.#obstacleRows = obstacleRows;
-        this.#obstacleColumns = obstacleColumns;
-        this.#obstaclesByRow = obstaclesByRow;
-        this.#obstaclesByColumn = obstaclesByColumn;
-
-        console.log({ obstacleRows, obstacleColumns, obstaclesByRow, obstaclesByColumn });
-        console.log(this.#pos, this.#dir);
     }
 }
